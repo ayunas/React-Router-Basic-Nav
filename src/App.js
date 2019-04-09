cf1a632886c311905981ac2c0d76a0c3bde2aa4a
ayunas <ayunas@gmail.com>
2019-04-08 23:19:46 -0500

    wrapped the App in BrowserRouter in the ReactDOM.render.  Created three Routes using 3 different components Home About and Contact and create three Links for them in the Navigation  Placed the Navigation in the App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
+
+
 
 const App = () => (
+
   <div>
-    <Navigation />
-  </div>
+    <Navigation/>
+    <Route path='/' exact component={Home}> Home </Route>
+    <Route path='/about' component={About}> About </Route>
+    <Route path='/contact' component={Contact}> Contact </Route>
+  </div>  
 );
 
 export default App;
